e94c14cf9eddc8a978a75082ce0cb71b630ce3f7
vagrant <vagrant@localhost.localdomain>
2021-02-11 14:29:40 +0000

    javascript_2/11

---
+++ while.js
@@ -0,0 +1,16 @@
+// Boolean型の値がtrueの間、処理が繰り返される
+
+while (Boolean型の値){
+    // 処理
+}
+
+// 下記の状態だと、処理を無限に繰り返すのでpcがフリーズしてしまう。
+// while (true){
+// console.log("繰り返す");
+// }
+
+let x = 0;
+while (x < 10){
+console.log("現在の値は${x}です。");
+x++;
+}
